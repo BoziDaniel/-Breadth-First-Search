d9c486594fc58ff2ee8ab4ce06d2ac33434f345f
BoziDaniel <daniel.bozi88@gmail.com>
2020-05-17 15:56:40 +0200

    initial repo

---
+++ src/main/java/com/codecool/bfsexample/BFSExample.java
@@ -0,0 +1,23 @@
+package com.codecool.bfsexample;
+
+import com.codecool.bfsexample.model.UserNode;
+
+import java.util.*;
+
+public class BFSExample {
+
+    private static void populateDB() {
+
+        RandomDataGenerator generator = new RandomDataGenerator();
+        List<UserNode> users = generator.generate();
+
+        GraphPlotter graphPlotter = new GraphPlotter(users);
+        
+        System.out.println("Done!");
+    }
+
+    public static void main(String[] args) {
+        populateDB();
+
+    }
+}
