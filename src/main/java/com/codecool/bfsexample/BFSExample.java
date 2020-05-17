6159f9656c76e141c1b9054bfe3f79ff2464ddf6
BoziDaniel <daniel.bozi88@gmail.com>
2020-05-17 16:59:33 +0200

    Added a highlighter to actual and searched user for improved usefulness

--- src/main/java/com/codecool/bfsexample/BFSExample.java
+++ src/main/java/com/codecool/bfsexample/BFSExample.java
@@ -4,20 +4,37 @@ import com.codecool.bfsexample.model.UserNode;
 
 import java.util.*;
 
+
 public class BFSExample {
 
-    private static void populateDB() {
+    private static List<UserNode> populateDB() {
 
         RandomDataGenerator generator = new RandomDataGenerator();
         List<UserNode> users = generator.generate();
-
-        GraphPlotter graphPlotter = new GraphPlotter(users);
-        
+        System.out.println(users.toString());
         System.out.println("Done!");
+        return users;
     }
 
     public static void main(String[] args) {
-        populateDB();
+        List<UserNode> users = populateDB();
 
+        UserNode emeryJohnoson = users.get(0);
+        UserNode anotherUser = findUser("Hadley_Kibo", users);
+        Set<UserNode> anotherUsers = new HashSet<>();
+        anotherUsers.add(anotherUser);
+        GraphPlotter graphPlotter = new GraphPlotter(users);
+        graphPlotter.highlightNodes(anotherUsers, emeryJohnoson);
+        int distance = emeryJohnoson.getDistanceBetweenUsers(anotherUser.getFullName(), users);
+        System.out.println( "distance: " + distance);
+    }
+
+    private static UserNode findUser(String username, List<UserNode> users) {
+        for (UserNode user : users) {
+            if (user.getFullName().equals(username)) {
+                return user;
+            }
+        }
+        return null;
     }
 }
--- src/main/java/com/codecool/bfsexample/model/UserNode.java
+++ src/main/java/com/codecool/bfsexample/model/UserNode.java
@@ -18,10 +18,17 @@ public class UserNode {
         idCounter++;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.fullName = firstName + "_" + lastName;
     }
 
     public long getId() {
         return id;
+    }
+
+    public String fullName;
+
+    public String getFullName() {
+        return fullName;
     }
 
     public Set<UserNode> getFriends() {return friends;}
@@ -46,7 +53,35 @@ public class UserNode {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+    public Integer getDistanceBetweenUsers(String fullname, List<UserNode> users) {
+        Integer counter = 0;
+        Queue<UserNode> usersToCheck = new LinkedList<>();
+        Set<UserNode> checkedUsers = new HashSet<>();
+        usersToCheck.add(this);
+        if (this.fullName.equals(fullname)) {
+            return counter;
+        }
+        while (!usersToCheck.isEmpty()) {
+            counter++;
+            UserNode actualUser;
+            System.out.println("usersToCheck: " + usersToCheck.toString());
+            actualUser = usersToCheck.poll();
+            System.out.println("actualUser: " + actualUser );
+            if (actualUser.fullName.equals(fullname)) {
+                return counter;
+            }
+            Set<UserNode> friends = actualUser.getFriends();
+            for (UserNode friend : friends) {
+                if (!usersToCheck.contains(friend) || !checkedUsers.contains(friend)) {
+                    usersToCheck.add(friend);
+                }
+            }
+            checkedUsers.add(actualUser);
+            System.out.println("checkedusers:" + checkedUsers.toString());
+        }
+        return -1;
 
+    }
     public String toString() {
         return firstName + "_" + lastName + " (" + id + ")";
     }
